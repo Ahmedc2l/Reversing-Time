b5672957da009c7132f9abbed3de0f9f5148e732
Ahmed Fathy <am.fathy396@gmail.com>
2020-01-15 13:34:57 +0200

    initial commit

---
+++ Assets/Scripts/Shooter.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooter : MonoBehaviour {
+	public Rigidbody projectile;
+	public Transform shotPos;
+	public float shotForce = 2000f;
+	public float moveSpeed = 10f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float h = Input.GetAxis ("Horizontal");
+		float v = Input.GetAxis ("Vertical");
+
+		transform.Translate (new Vector3 (h, v, 0f) * Time.deltaTime * moveSpeed);
+
+		if (Input.GetButtonUp ("Fire1")) {
+			Rigidbody shot = Instantiate (projectile, shotPos.position, shotPos.rotation) as Rigidbody;
+			shot.AddForce (shotPos.forward * shotForce);
+			Destroy(shot.gameObject,2f);
+		}
+	}
+}
---
+++ Assets/Scripts/TimeBody.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeBody : MonoBehaviour {
+
+	bool isRewinding = false;
+
+	public float recordTime = 5f;
+
+	List<PointInTime> pointsInTime;
+
+	Rigidbody rb;
+
+	void Start () {
+		pointsInTime = new List<PointInTime>();
+		rb = GetComponent<Rigidbody>();
+	}
+
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Return))
+			StartRewind();
+		if (Input.GetKeyUp(KeyCode.Return))
+			StopRewind();
+	}
+
+	void FixedUpdate ()
+	{
+		if (isRewinding)
+			Rewind();
+		else
+			Record();
+	}
+
+	void Rewind ()
+	{
+		if (pointsInTime.Count > 0)
+		{
+			transform.position = pointsInTime[0].position;
+			transform.rotation = pointsInTime[0].rotation;
+			pointsInTime.RemoveAt(0);
+		} 
+		else
+		{
+			StopRewind();
+		}
+
+	}
+
+	void Record ()
+	{
+		if (pointsInTime.Count > Mathf.Round(recordTime / Time.fixedDeltaTime))
+		{
+			// Remove from the bottom
+			pointsInTime.RemoveAt(pointsInTime.Count - 1);
+		}
+
+		// Add to the Top
+		pointsInTime.Insert(0, new PointInTime(transform.position, transform.rotation));
+	}
+
+	public void StartRewind ()
+	{
+		isRewinding = true;
+		//Stop unity from applying physics
+		rb.isKinematic = true;
+	}
+
+	public void StopRewind ()
+	{
+		isRewinding = false;
+		rb.isKinematic = false;
+	}
+}
